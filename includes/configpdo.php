1053d783748a0b01417e4632ac1dd6f6e164940a
Phạm Nguyên Bảo <pnb6423@gmail.com>
2024-07-14 20:22:31 +0700

    qldsv

---
+++ includes/configpdo.php
@@ -0,0 +1,8 @@
+<?php
+$dbuser="b7_36095224";
+$dbpass="noboom909";
+$host="sql302.byethost7.com";
+$dbname = "b7_36095224_srms";
+$mysqli = new mysqli($host, $dbuser, $dbpass, $dbname);
+
+?>
---
+++ includes/leftbar.php
@@ -0,0 +1,62 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="images/avatar.jpg" alt="John Doe" class="img-circle profile-img">
+                                <h6 class="title">ADMIN</h6>
+                                <small class="info">Quản Lý Hệ Thống Điểm Sinh Viên</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Danh Mục Chính</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Bảng Điều Khiển</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Chức Năng</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Lớp Học Phần</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>Tạo Lớp Học Phần</span></a></li>
+                                            <li><a href="manage-classes.php"><i class="fa fa fa-server"></i> <span>Quản Lý Lớp</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Môn Học</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-bars"></i> <span>Tạo Môn Học</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Quản Lý Môn Học</span></a></li>
+                                           <li><a href="add-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Thêm Kết Hợp Môn Và Lớp</span></a></li>
+                                           <a href="manage-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Quản Lý Kết Hợp Môn Và Lớp </span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Sinh Viên</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Thêm Sinh Viên</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Quản Lý Sinh Viên</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Kết Quả</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>Thêm Kết Quả</span></a></li>
+                                            <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>Quản Lý Kết Quả</span></a></li>
+                                           
+                                        </ul>
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span> Thay Đổi Mật Khẩu Admin</span></a></li>
+                                           
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
